496bf0bf26b82c4f5824c2a5a28e3b7686700773
Segun <segun.aremu@outlook.com>
2024-01-04 16:40:16 -0800

    6. Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 import sys
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-args = sys.argv
-if len(args) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+    a = int(args[1])
+    operator = args[2]
+    b = int(args[3])
 
-a = int(args[1])
-operator = args[2]
-b = int(args[3])
+    operations = {'+': add, '-': sub, '*': mul, '/': div}
 
-operations = {'+': add, '-': sub, '*': mul, '/': div}
+    if operator not in operations:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-if operator not in operations:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
-
-result = operations[operator](a, b)
-print(f"{a} {operator} {b} = {result}")
+    result = operations[operator](a, b)
+    print(f"{a} {operator} {b} = {result}")
