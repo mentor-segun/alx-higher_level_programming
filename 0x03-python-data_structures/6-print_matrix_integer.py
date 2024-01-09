273ca6ffa6afcdbac837b0eeacf6435bc2d55384
Segun <segun.aremu@outlook.com>
2024-01-09 09:28:43 -0800

    6. Lists of lists = Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for r in matrix:
+        for c, number in enumerate(r):
+            if c != len(r) - 1:
+                print("{:d} ".format(num), end='')
+            else:
+                print("{:d}".format(num))
+                
+        if matrix:
+        print("--")
